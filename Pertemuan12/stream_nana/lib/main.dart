ad7908efa6a8cca82b2b158843a5cf312e7823e6
FaradillaRoudhotulSanaa <faradillaroudhotul@gmail.com>
2024-11-25 18:06:35 +0700

    W12: Jawaban Soal 1

---
+++ Pertemuan12/stream_nana/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream Nana',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const StreamHomePage(),
+    );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _StreamHomePageState();
+}
+class _StreamHomePageState extends State<StreamHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
